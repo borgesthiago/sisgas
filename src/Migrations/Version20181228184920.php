1b08c9e728901529003ffe846eb55f1b84a917c5
borgesthiago <thiagomat25@gmail.com>
2019-05-30 09:57:50 -0300

    Adição de usuario padrao

---
+++ src/Migrations/Version20181228184920.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181228184920 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE equipamento ADD endereco VARCHAR(255) DEFAULT NULL, ADD bairro VARCHAR(100) DEFAULT NULL, ADD telefone VARCHAR(20) DEFAULT NULL, ADD email VARCHAR(100) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE equipamento DROP endereco, DROP bairro, DROP telefone, DROP email');
+    }
+}
